26eddfe07a6bb4af51cdd1b67ed2679d3de254aa
Patrick Garrett Pavesi <patrickgpavesi@gmail.com>
2015-07-10 15:42:53 -0400

    removed fabricators from db/seed.rb. issues with deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-test_user = Fabricate(:user, full_name: 'Test Testington')
-
 Comic.create!(title: 'The Amazing Spider-Man', issue_number: 121, cover_art_url: 'https://upload.wikimedia.org/wikipedia/en/2/24/Asm121.jpg' )
 
 Comic.create!(title: 'Batman: The Long Halloween', issue_number: 1, cover_art_url: 'https://upload.wikimedia.org/wikipedia/en/5/52/Batman_thelonghalloween.jpg')
@@ -25,11 +23,3 @@ Comic.create!(title: 'Iron Man', issue_number: 1, cover_art_url: 'http://img1.wi
 Comic.create!(title: 'The Crow', issue_number: 1, cover_art_url: 'https://upload.wikimedia.org/wikipedia/en/8/85/The_Crow1_Cover.jpg')
 
 Comic.create!(title: 'The Killing Joke', issue_number: 1, cover_art_url: 'https://upload.wikimedia.org/wikipedia/en/3/32/Killingjoke.JPG')
-
-test_discussion = Fabricate(:discussion, comic: superman, creator: test_user, title: 'Why Superman Is Lame', body: "He's not Batman ;-)")
-
-test_discussion2 = Fabricate(:discussion, comic: superman, creator: test_user, title: 'Flash v. Doomsday', body: "Superman got beat pretty badly in this storyline. Do you think the Flash would've put up a more interesting fight?")
-
-bruce = Fabricate(:user, full_name: 'Bruce Wayne')
-
-Fabricate(:reply, body: "Couldn't agree with you more.", discussion: test_discussion, creator: bruce)
